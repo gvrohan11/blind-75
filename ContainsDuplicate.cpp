17ead866184efd355baff838c40b1c5ba5e23562
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-08-23 21:37:22 -0500

    Create ContainsDuplicate.cpp

---
+++ ContainsDuplicate.cpp
@@ -0,0 +1,22 @@
+// Hashset - O(n)
+// Sorting - O(n) if sorted; O(nlog(n)) if not
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        // Hashset solution
+        set<int> set;
+        for (int i : nums) {
+            if (set.find(i) != set.end()) return true;
+            else set.insert(i);
+        }
+        return false;
+        
+        /* Sorting solution 
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) if (nums[i] == nums[i + 1]) return true;
+        return false;
+        */
+    }
+};
