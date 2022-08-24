448a2602c9c1e66979a21cbd1e3a7846cd96e157
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-08-23 21:20:53 -0500

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,19 @@
+// O(nlog(n))
+// If Sorted - O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::sort(nums.begin(), nums.end());
+        int l = 0;
+        int r = nums.size() - 1;
+        // vector<int> vect;
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (sum > target) r--;
+            else if (sum < target) l++;
+            else break;
+        }
+        return {l, r};
+    }
+};
